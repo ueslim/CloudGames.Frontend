771a579d30b8600b5bfbf7cd4c8645329df13b00
ueslim <107502335+ueslim@users.noreply.github.com>
2025-10-08 13:32:14 -0300

    correcao rota no environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  USERS_API: 'https://cloudgames-apim.azure-api.net/users',
-  GAMES_API: 'https://cloudgames-apim.azure-api.net/games',
-  PAYMENTS_API: 'https://cloudgames-apim.azure-api.net/payments',
+  USERS_API: 'https://cloudgames-apim.azure-api.net/api/users',
+  GAMES_API: 'https://cloudgames-apim.azure-api.net/api/games',
+  PAYMENTS_API: 'https://cloudgames-apim.azure-api.net/api/payments'
 };
 
 
