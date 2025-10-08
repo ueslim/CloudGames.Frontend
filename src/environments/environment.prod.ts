a57b4a6072d8e0ab804d3af60cd87361da6558b8
ueslim <107502335+ueslim@users.noreply.github.com>
2025-10-08 13:15:49 -0300

    ajuste de rota para api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: true,
-  USERS_API: 'https://cloudgames-apim.azure-api.net/users',
-  GAMES_API: 'https://cloudgames-apim.azure-api.net/games',
-  PAYMENTS_API: 'https://cloudgames-apim.azure-api.net/payments',
-  APIM_SUBSCRIPTION_KEY: '', // Will be set via Azure Static Web Apps environment variables
+  USERS_API: 'https://cloudgames-apim.azure-api.net/api/users',
+  GAMES_API: 'https://cloudgames-apim.azure-api.net/api/games',
+  PAYMENTS_API: 'https://cloudgames-apim.azure-api.net/api/payments',
+  APIM_SUBSCRIPTION_KEY: '',
 };
-
-
